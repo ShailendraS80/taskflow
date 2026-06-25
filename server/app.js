9083beaee4fd512a3bf6fb0535fc28ae56a816bf
Shailendra Sharma <shailendrasharma4968@gmail.com>
2026-06-25 23:46:07 +0530

    Setup backend foundation and MongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    message: "TaskFlow API is running 🚀",
+    message: "TaskFlow API Running 🚀",
   });
 });
 
